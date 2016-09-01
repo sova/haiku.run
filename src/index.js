46c275e4dda30cfb301ac1af157618904577c464
vas <sova.kuliana@gmail.com>
2016-08-31 23:27:13 -0600

    nestled striptags inwards
    haiku is JSON haiku.line1 is strippable et.

--- src/index.js
+++ src/index.js
@@ -21,8 +21,7 @@ io.on('connection', function(socket) {
     });
 
     socket.on('post_haiku', function(haiku) {
-        haiku = striptags(haiku)
-        var haiku_html = '<li class="line1">' + haiku.line1 + '</li><li class="line2">' + haiku.line2 + '</li><li class="line3">' + haiku.line3 + "</li><div class='haiku_author'>" + client_ip + "</div> \n";
+        var haiku_html = '<li class="line1">' + striptags(haiku.line1) + '</li><li class="line2">' + striptags(haiku.line2) + '</li><li class="line3">' + striptags(haiku.line3) + "</li><div class='haiku_author'>" + client_ip + "</div> \n";
         io.emit('share_haiku', haiku_html);
         fs.appendFile('haiku-2016-09', haiku_html, function(err) {});
         console.log('new haiku from ' + client_ip + ': ' + haiku.line1 + "/ " + haiku.line2 + "/ " + haiku.line3);
