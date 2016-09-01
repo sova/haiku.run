62207e79ca2903b77613738176ab5c972f47474e
vas <sova.kuliana@gmail.com>
2016-08-31 20:45:20 -0600

    haiku.run looks close!
    weird doubling and tripling and quadrupling on submissions but it’s
    coming together!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 app.get('/', function(req, res) {
     res.sendfile('views/index.html');
 });
 
 io.on('connection', function(socket) {
-    console.log('a user connected');
+    var socket_id = socket.id;
+    var client_ip = socket.request.connection.remoteAddress;
+
+    console.log('user [' + client_ip + '] connected');
 
     //when a socket is disconnected or closed, .on('disconnect') is fired
     socket.on('disconnect', function() {
@@ -18,9 +22,12 @@ io.on('connection', function(socket) {
 
 
 io.on('connection', function(socket) {
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
-        //console.log('message: ' + msg);
+    var client_ip = socket.request.connection.remoteAddress;
+    socket.on('haiku', function(haiku) {
+        var haiku_html = '<li class="line1">' + haiku.line1 + '</li><li class="line2">' + haiku.line2 + '</li><li class="line3">' + haiku.line3 + "</li>\n";
+        io.emit('haiku', haiku_html);
+        fs.appendFile('haiku-2016-09', haiku_html, function(err) {});
+        console.log('new haiku from ' + client_ip + ': ' + haiku.line1 + "/ " + haiku.line2 + "/ " + haiku.line3);
     });
 });
 
